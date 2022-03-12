7278a9cf257f3596a401f12279c94a64004eff3a
Adnan Sait <adnansait74@gmail.com>
2022-03-12 20:51:19 +0530

    increased range of task data validation formula

--- utilities/Constants.py
+++ utilities/Constants.py
@@ -1,4 +1,4 @@
 class Constants:
 
     TASK_ENUM = {"kt": "Knowledge_Transfer"}
-    TASK_DATA_VALIDATION = "=OFFSET(\'Project-Task Mapping\'!$A$2:$BB$20, 0, MATCH($C2, projectNames, 0)-1, COUNTA(OFFSET(\'Project-Task Mapping\'!$A$2:$BB$20, 0, MATCH($C2, projectNames, 0)-1, 20, 1)), 1)"
+    TASK_DATA_VALIDATION = "=OFFSET(\'Project-Task Mapping\'!$A$2:$BB$999, 0, MATCH($C2, projectNames, 0)-1, COUNTA(OFFSET(\'Project-Task Mapping\'!$A$2:$BB$999, 0, MATCH($C2, projectNames, 0)-1, 999, 1)), 1)"
